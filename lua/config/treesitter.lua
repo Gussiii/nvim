2f9e94c32e22d67ac8855a2bfe35b90c2ac4e171
Gussiii <49954168+Gussiii@users.noreply.github.com>
2022-12-16 18:33:24 -0600

    Add markdowk to treesitter

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,6 +1,6 @@
 local configs = require("nvim-treesitter.configs")
 configs.setup {
-  ensure_installed = {"lua","python","julia"},
+  ensure_installed = {"lua","python","julia","markdown"},
   sync_install = false, 
   ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
