eb43f11374ad20beb2ad4bb1b4b57b7819fcee0a
Agustin Rodriguez (TK-M-31) <agustin.rodriguez@bmw.com.mx>
2024-01-26 08:01:59 -0600

    Lazy git

--- lua/config/color.lua
+++
@@ -1,8 +0,0 @@
-return 	{
-  "folke/tokyonight.nvim",
-  -- ensure that the color scheme is loaded at the very beginning
-  lazy = false,
-  priority = 1000,
-  -- enable the colorscheme
-  config = function() vim.cmd.colorscheme("tokyonight") end,
-}
